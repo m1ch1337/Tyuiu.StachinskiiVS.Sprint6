b318896266ba10eba1dc4d8bdcc64d60cacd2617
StachinskiiVS <boss20276@gmail.com>
2026-03-22 23:53:43 +0500

    Task7

---
+++ Tyuiu.StachinskiiVS.Sprint6.Task7.V25.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.StachinskiiVS.Sprint6.Task7.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetMatrix()
+        {
+            string filePath = @"C:\Users\stach\source\repos\Tyuiu.StachinksiiVS.Sprint6\Tyuiu.StachinksiiVS.Sprint6.Task7.V2\bin\Debug\InPutFileTask7V25.csv";
+
+            FileInfo fileInfo = new FileInfo(filePath);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
